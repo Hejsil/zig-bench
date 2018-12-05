36d08f3e18615a95a54cbe498c74ce233a8d04e9
Jimmi Holst Christensen <jhc@liab.dk>
2018-12-05 12:06:50 +0100

    Initial impl

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # zig-bench
+
+A simple benchmarking lib in Zig
+
+```
+Test 1/1 benchmark...
+Benchmark     Arg             Mean(ns)
+--------------------------------------
+sum_slice       0                   13
+sum_slice       1                   14
+sum_slice       2                   14
+sum_slice       3                   14
+sum_slice       4                   14
+sum_slice       5                   13
+sum_stream      0                   18
+sum_stream      1                   30
+sum_stream      2                   42
+sum_stream      3                   67
+sum_stream      4                  118
+sum_stream      5                  211
+OK
+All tests passed.
+```
---
+++ src/index.zig
@@ -0,0 +1,172 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+const debug = std.debug;
+const io = std.io;
+const math = std.math;
+const mem = std.mem;
+const meta = std.meta;
+const os = std.os;
+const time = os.time;
+
+const Def = builtin.TypeInfo.Definition;
+
+pub fn benchmark(comptime B: type) !void {
+    if (getDef(B, "setup")) |_|
+        _ = B.setup();
+
+    const args = if (getDef(B, "args")) |_| B.args else []void{{}};
+    const iterations: u32 = if (getDef(B, "iterations")) |_| B.iterations else 100000;
+    const functions = comptime blk: {
+        var res: []const Def = []Def{};
+        for (meta.definitions(B)) |def| {
+            if (comptime mem.eql(u8, def.name, "setup"))
+                continue;
+            if (def.data != builtin.TypeInfo.Definition.Data.Fn)
+                continue;
+
+            res = res ++ []Def{def};
+        }
+
+        break :blk res;
+    };
+    if (functions.len == 0) {
+        debug.warn("\nNo benchmarks to run.\n");
+    }
+
+    const max_fn_name_len = comptime max(Def, functions, defNameLessThan).?.name.len;
+    const max_name_spaces = math.max(max_fn_name_len, "Benchmark".len);
+    const max_args_spaces = math.max(args.len, "Arg".len);
+
+    var timer = try time.Timer.start();
+    debug.warn("\n");
+    debug.warn("Benchmark");
+    nTimes(' ', (max_name_spaces - "Benchmark".len) + 1);
+    nTimes(' ', max_args_spaces - "Arg".len);
+    debug.warn("Arg ");
+    nTimes(' ', digits(u64, 10, math.maxInt(u64)) - "Mean(ns)".len);
+    debug.warn("Mean(ns)\n");
+    nTimes('-', max_name_spaces + max_args_spaces + digits(u64, 10, math.maxInt(u64)) + 2);
+    debug.warn("\n");
+
+    inline for (functions) |def| {
+        for (args) |arg, index| {
+            var runtime_sum: u128 = 0;
+
+            var i: usize = 0;
+            while (i < iterations) : (i += 1) {
+                timer.reset();
+
+                switch (@typeOf(arg)) {
+                    void => _ = @noInlineCall(@field(B, def.name)),
+                    else => _ = @noInlineCall(@field(B, def.name), arg),
+                }
+
+                const runtime = timer.read();
+                runtime_sum += runtime;
+            }
+
+            const runtime_mean = @intCast(u64, runtime_sum / iterations);
+
+            debug.warn("{}", def.name);
+            nTimes(' ', (max_name_spaces - def.name.len) + 1);
+            nTimes(' ', max_args_spaces - digits(usize, 10, index));
+            debug.warn("{} ", index);
+            nTimes(' ', digits(u64, 10, math.maxInt(u64)) - digits(u64, 10, runtime_mean));
+            debug.warn("{}\n", runtime_mean);
+        }
+    }
+}
+
+fn digits(comptime N: type, comptime base: comptime_int, n: N) usize {
+    comptime var res = 1;
+    comptime var check = base;
+
+    inline while (check <= math.maxInt(N)) : ({
+        check *= base;
+        res += 1;
+    }) {
+        if (n < check)
+            return res;
+    }
+
+    return res;
+}
+
+fn nTimes(c: u8, times: usize) void {
+    var i: usize = 0;
+    while (i < times) : (i += 1)
+        debug.warn("{c}", c);
+}
+
+fn getDef(comptime T: type, name: []const u8) ?builtin.TypeInfo.Definition {
+    for (@typeInfo(T).Struct.defs) |def| {
+        if (mem.eql(u8, def.name, name))
+            return def;
+    }
+
+    return null;
+}
+
+fn max(comptime T: type, slice: []const T, lessThan: fn (T, T) bool) ?T {
+    const i = maxIndex(T, slice, lessThan) orelse return null;
+    return slice[i];
+}
+
+fn maxIndex(comptime T: type, slice: []const T, lessThan: fn (T, T) bool) ?usize {
+    if (slice.len == 0)
+        return null;
+
+    var res: usize = 0;
+    for (slice[1..]) |_, i| {
+        if (lessThan(slice[res], slice[i + 1]))
+            res = i + 1;
+    }
+
+    return res;
+}
+
+fn defNameLessThan(comptime a: Def, comptime b: Def) bool {
+    return mem.lessThan(u8, a.name, b.name);
+}
+
+test "benchmark" {
+    try benchmark(struct {
+        // The functions will be benchmarked with the following inputs.
+        // If not present, then it is assumed that the functions
+        // take no input.
+        var args = [][]const u8{
+            []u8{},
+            []u8{ 1, 10, 100 } ** 1,
+            []u8{ 1, 10, 100 } ** 2,
+            []u8{ 1, 10, 100 } ** 4,
+            []u8{ 1, 10, 100 } ** 8,
+            []u8{ 1, 10, 100 } ** 16,
+        };
+
+        // How many iterations to run each benchmark.
+        // If not present then a default will be used.
+        const iterations = 1000000;
+
+        // If present, setup will be called before any benchmarks are run.
+        fn setup() void {}
+
+        fn sum_slice(slice: []const u8) u64 {
+            var res: u64 = 0;
+            for (slice) |item|
+                res += item;
+
+            return res;
+        }
+
+        fn sum_stream(slice: []const u8) u64 {
+            var stream = &io.SliceInStream.init(slice).stream;
+            var res: u64 = 0;
+            while (stream.readByte()) |c| {
+                res += c;
+            } else |_| {}
+
+            return res;
+        }
+    });
+}
