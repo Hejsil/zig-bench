0e1c1bc414924bb7575b81782c7e190f4a29b938
Jimmi HC <jimmiholstchristensen@gmail.com>
2019-03-04 20:10:05 +0100

    Added travis yml and updated to newest lib convention

---
+++ build.zig
@@ -0,0 +1,36 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+const Mode = builtin.Mode;
+const Builder = std.build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const test_all_step = b.step("test", "Run all tests in all modes.");
+    inline for ([]Mode{ Mode.Debug, Mode.ReleaseFast, Mode.ReleaseSafe, Mode.ReleaseSmall }) |test_mode| {
+        const mode_str = comptime modeToString(test_mode);
+
+        const tests = b.addTest("bench.zig");
+        tests.setBuildMode(test_mode);
+        tests.setNamePrefix(mode_str ++ " ");
+
+        const test_step = b.step("test-" ++ mode_str, "Run all tests in " ++ mode_str ++ ".");
+        test_step.dependOn(&tests.step);
+        test_all_step.dependOn(test_step);
+    }
+
+    const all_step = b.step("all", "Build everything and runs all tests");
+    all_step.dependOn(test_all_step);
+
+    b.default_step.dependOn(all_step);
+}
+
+fn modeToString(mode: Mode) []const u8 {
+    return switch (mode) {
+        Mode.Debug => "debug",
+        Mode.ReleaseFast => "release-fast",
+        Mode.ReleaseSafe => "release-safe",
+        Mode.ReleaseSmall => "release-small",
+    };
+}
